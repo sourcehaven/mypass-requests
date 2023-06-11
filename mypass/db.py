f852cb89855ccf4989ab04da3aba9d3e621300a0
skyzip2142 <skyzip96@gmail.com>
2023-06-11 13:47:06 +0200

    Passing host and port to logout;

--- mypass/db.py
+++ mypass/db.py
@@ -12,7 +12,7 @@ from .exceptions import MyPassRequestException
 @bind_app_config()
 def create_master_pw_with_login(pw: str, host: str = None, port: int = None) -> int:
     url = f'{host}/api/db/master/create'
-    current_app().config.login(pw)
+    current_app().config.login(pw, host=host, port=port)
     auth_token = current_app().config.session['access_token']
     headers = {'authorization': f'Bearer {auth_token}'}
     proxies = gen_proxy_from_port(host, port)
@@ -55,8 +55,8 @@ def update_master_pw(pw: str, host: str = None, port: int = None, auth_token: st
     resp = requests.post(url=url, json=json_obj, headers=headers, proxies=proxies)
     if resp.status_code == 200:
         try:
-            current_app().config.logout()
-            current_app().config.login(pw)
+            current_app().config.logout(host=host, port=port)
+            current_app().config.login(pw, host=host, port=port)
         except (ValueError, TypeError):
             logging.getLogger().warning(
                 'USER WARNING :: Failed to logout and back inside api. Callbacks not configured.')
